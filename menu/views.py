3b79052d54be6e4ae1c90d961b0a1ebbeed239d8
Yan <yan.fonseca@gmail.com>
2020-12-09 03:57:02 -0300

    mostra receitas atraves do clique na figura, ainda incompleto

--- menu/views.py
+++ menu/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Recipe
 
 def home(request):
 
-    recipes = {
-        1:'Lasanha',
-        2: 'Sopa de Legumes',
-        3: 'Sorvete',
-        4: 'Bolo de chocolate'
-        }
-
+    recipes = Recipe.objects.all()
     context = {
-        'recipe_names': recipes
+        'recipes': recipes
     }
 
     return render(request,'index.html', context)
 
 def receita(request):
     return render(request, 'receita.html')
+
+# You can use this code if you don't have a database created with postgresql
+
+# def home(request):
+
+#     recipes = {
+#         1:'Lasanha',
+#         2: 'Sopa de Legumes',
+#         3: 'Sorvete',
+#         4: 'Bolo de chocolate'
+#         }
+
+#     context = {
+#         'recipe_names': recipes
+#     }
+
+#     return render(request,'index.html', context)
