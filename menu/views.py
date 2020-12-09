75094b3f43cbf069c8a5efb237986b0fc1a6583f
Yan <yan.fonseca@gmail.com>
2020-12-09 01:38:36 -0300

    Receita pode ser adicionada dinamicamente no html

--- menu/views.py
+++ menu/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+
+    recipes = {
+        1:'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete',
+        4: 'Bolo de chocolate'
+        }
+
+    context = {
+        'recipe_names': recipes
+    }
+
+    return render(request,'index.html', context)
 
 def receita(request):
     return render(request, 'receita.html')
