bb06273f4cdf0f5fe846212e973e49170a8a92ef
Yan <yan.fonseca@gmail.com>
2020-12-09 03:19:08 -0300

    Criaçao do modelo para ser consumido pelo site. Instalados o pycopg2 e o psycopg2-binary necessarios para rodar o postgresql. makemigrations e migrate feitos. Tabela criada no banco de dados recipe. Alteraçao em settigns para se conectar ao postgres.

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    ingredient = models.TextField()
+    preparation = models.TextField()
+    preparation_time = models.IntegerField()
+    portions = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    created_at = models.DateField(default=datetime.now, blank=True)
+
+
