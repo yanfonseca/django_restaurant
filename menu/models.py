25897817d05986f2ecca0f95b90677633b96f9e7
Yan <yan.fonseca@gmail.com>
2020-12-10 19:27:24 -0300

    Feito o link de pessoa com receita. Mostra o nome da pessoa no html.

--- menu/models.py
+++ menu/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 
+from people.models import PeopleModel
+
 class Recipe(models.Model):
+
+    person = models.ForeignKey(
+        PeopleModel, 
+        on_delete=models.CASCADE,
+        default='')
+
     name = models.CharField(max_length=200)
     ingredient = models.TextField()
     preparation = models.TextField()
--- people/models.py
+++ people/models.py
@@ -7,4 +7,5 @@ class PeopleModel(models.Model):
     name = models.CharField(max_length=200)
     email= models.EmailField(max_length=200)
 
-    
+    def __str__(self):
+        return self.name
