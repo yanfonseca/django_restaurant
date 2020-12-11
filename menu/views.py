d7a55e7b078d3d7e75350619ebe6def9cb24a943
Yan <yan.fonseca@gmail.com>
2020-12-11 16:20:09 -0300

    Realiza busca e retorna receita.

--- menu/views.py
+++ menu/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 
 def search(request):
-    return render(request, 'buscar.html')
+    print(request)
+
+    recipes = Recipe.objects.order_by('-created_at').filter(publish=True)
+
+    if 'xxsearch' in request.GET:
+        print('passou 1')
+        search_filter = request.GET['xxsearch']
+
+        print(search)
+        # if 'xxsearch':
+        print('passou 2')
+        recipes = recipes.filter(name__icontains=search_filter)
+
+    context = {
+        'recipes':recipes
+    }
+    
+    return render(request, 'buscar.html', context)
 
 
 def home(request):
