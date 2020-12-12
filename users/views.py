61c676ec9ebf8c180428c3b4df045063b235ab54
Yan <yan.fonseca@gmail.com>
2020-12-12 02:43:48 -0300

    Criado app users. Dentro de users url.py as rotas ja foram criadas e molde das views

---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def subscribe(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def dashboard(request):
+    pass
+
+
+def lougout(request):
+    pass
+
