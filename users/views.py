ba650ab0e92d3c407f829480f1dfec0e7d377ea5
Yan <yan.fonseca@gmail.com>
2020-12-15 22:43:38 -0300

    Cria login e logout.

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth.models import User
 
+from django.contrib import auth
 # Create your views here.
 
 
@@ -46,13 +47,38 @@ def subscribe(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            print(email, senha)
+            print("Os campos email e senha não podem ficar em branco")
+            return redirect('login')
+
+        print(email, senha)
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            print(nome)
+
+            if user is not None:
+                auth.login(request, user)
+                print('login realizado com sucesso')
+            return redirect('dashboard')
+
     return render(request, 'usuarios/login.html')
 
 
 def dashboard(request):
-    pass
+
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    return redirect('home')
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('home')
 
+def cria_receita(request):
+    return render(request, 'usuarios/cria_receita.html')
